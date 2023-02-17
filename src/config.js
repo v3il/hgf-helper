262ed38d097d85fe95ec28c052991dff4af388c0
Dmitry <v3il@ukr.net>
2023-02-17 21:39:08 +0200

    Final fixes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = Object.freeze({
     commands: ['!hitsquad', '!battleroyale', '!gauntlet'],
-    intervalBetweenRounds: 3 * 60 * 1000,
+    intervalBetweenRounds: 4 * 60 * 1000,
     intervalBetweenCommands: 3000,
 })
