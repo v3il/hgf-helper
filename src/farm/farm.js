5e015b6c582e8b28e6dbe41bdc2346cd1677882c
Dmytro <v3il@ukr.net>
2023-05-14 15:18:31 +0300

    Fix login popup

--- src/farm/farm.js
+++ src/farm/farm.js
@@ -12,34 +12,40 @@ import { users } from './users';
 
 function getTwitchElements() {
     const userDropdownToggleEl = document.querySelector('[data-a-target="user-menu-toggle"]');
-
-    userDropdownToggleEl?.click();
-
     const chatInputEl = document.querySelector('[data-a-target="chat-input"]');
     const sendMessageButtonEl = document.querySelector('[data-a-target="chat-send-button"]');
     const chatContainerEl = document.querySelector('.chat-scrollable-area__message-container');
     const videoEl = document.querySelector('video');
-    const userNameEl = document.querySelector('[data-a-target="user-display-name"]');
-
-    userDropdownToggleEl?.click();
 
     return {
+        userDropdownToggleEl,
         chatInputEl,
         sendMessageButtonEl,
         chatContainerEl,
-        videoEl,
-        userNameEl
+        videoEl
     };
 }
 
+function getUserName(userDropdownToggleEl) {
+    userDropdownToggleEl.click();
+
+    const userNameEl = document.querySelector('[data-a-target="user-display-name"]');
+    userDropdownToggleEl.click();
+
+    return userNameEl?.textContent.toLowerCase();
+}
+
 async function runApp({
-    chatInputEl, sendMessageButtonEl, chatContainerEl, userNameEl
+    chatInputEl, sendMessageButtonEl, chatContainerEl, userDropdownToggleEl
 }) {
-    const userName = userNameEl.textContent.toLowerCase();
+    const userName = getUserName(userDropdownToggleEl);
+
+    if (!userName) {
+        return;
+    }
+
     const userConfig = users.find(({ name }) => name === userName);
     const twitchUser = TwitchUser.create(userConfig);
-
-    console.error(userConfig, twitchUser);
 
     WaiterService.create({ twitchUser });
 
