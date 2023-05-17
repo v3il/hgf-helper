ad8a46737a1dfb5c580c5a0c8488050b8bdd866b
Dmytro <v3il@ukr.net>
2023-05-17 23:05:37 +0300

    Add new user

--- src/farm/utils/generateDelay.js
+++ src/farm/utils/generateDelay.js
@@ -6,18 +6,14 @@ export function generateDelay(min, max) {
 
 export function generateMiniGameDelay() {
     const basePart = 10 * Timing.SECOND;
-    const randomPart = generateDelay(10 * Timing.SECOND, 4 * Timing.MINUTE);
-
-    console.error('Mini games', basePart + randomPart, Math.floor((basePart + randomPart) / 60));
+    const randomPart = generateDelay(10 * Timing.SECOND, 5 * Timing.MINUTE);
 
     return basePart + randomPart;
 }
 
 export function generateHitsquadDelay() {
     const basePart = Timing.MINUTE;
-    const randomPart = generateDelay(10 * Timing.SECOND, 10 * Timing.MINUTE);
-
-    console.error('Hitsquad', basePart + randomPart, Math.floor((basePart + randomPart) / 60));
+    const randomPart = generateDelay(10 * Timing.SECOND, 15 * Timing.MINUTE);
 
     return basePart + randomPart;
 }
