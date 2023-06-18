dd91789d8054109c68b2f6289432a50c95c65cac
Dmytro <v3il@ukr.net>
2023-06-18 15:01:09 +0300

    Edit limited hitsquad runner

--- src/farm/services/LimitedHitsquadRunner.js
+++ src/farm/services/LimitedHitsquadRunner.js
@@ -1,7 +1,57 @@
-export class LimitedHitsquadRunner {
-    #twitchCharObserver;
+import { Commands, MessageTemplates } from '../consts';
 
-    constructor({ twitchCharObserver }) {
-        this.#twitchCharObserver = twitchCharObserver;
+export class LimitedHitsquadRunner {
+    #twitchChatObserver;
+
+    #lastBattleRoyalTime = 0;
+    #lastHitsquadTimes = [0, 0, 0, 0, 0];
+
+    constructor({ twitchChatObserver }) {
+        this.#twitchChatObserver = twitchChatObserver;
+
+        this.#listenEvents();
+    }
+
+    #listenEvents() {
+        this.#twitchChatObserver.events.on('message', ({ userName, message }) => {
+            this.#processMessage({ userName, message });
+        });
+    }
+
+    #processMessage({ userName, message }) {
+        // if (message.includes(MessageTemplates.NEW_QUIZ_QUESTION)) {
+        //     this.#isWaitingNextRound = false;
+        //     this.#desiredAnswerPosition = this.#getDesiredAnswerPosition();
+        //
+        //     this.#resetAnswers();
+        //     this.#registerFallback();
+        // }
+        //
+        // if (this.#isWaitingNextRound) {
+        //     return;
+        // }
+        //
+        // const isMe = this.#twitchUser.isCurrentUser(userName);
+        // const answerInMessage = Commands.getAnswers().find((answer) => message.startsWith(answer));
+        //
+        // if (!answerInMessage) {
+        //     return;
+        // }
+        //
+        // if (isMe && answerInMessage) { // answered manually
+        //     return this.#completeRound();
+        // }
+        //
+        // if (this.#answeredUsers.includes(userName)) { // user answered again
+        //     return;
+        // }
+        //
+        // this.#answers[answerInMessage].add(userName);
+        // const { answer, answersCount } = this.#getCorrectAnswer();
+        //
+        // if (answersCount + 1 === this.#desiredAnswerPosition) {
+        //     console.error('send', answer, answersCount + 1);
+        //     this.#completeRound(answer);
+        // }
     }
 }
