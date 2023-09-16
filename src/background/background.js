2d1dd7314749aa9937477785bb650633cb8790b9
Dmytro <v3il@ukr.net>
2023-09-16 13:07:28 +0300

    Add onInstalled listener

--- src/background/background.js
+++ src/background/background.js
@@ -1,5 +1,16 @@
 import { SettingsService } from './SettingsService';
 
+chrome.runtime.onInstalled.addListener(async () => {
+    for (const contentScript of chrome.runtime.getManifest().content_scripts) {
+        for (const tab of await chrome.tabs.query({ url: contentScript.matches })) {
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: contentScript.js
+            });
+        }
+    }
+});
+
 (async () => {
     const settingsService = new SettingsService();
 
