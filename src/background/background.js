5b61d64d6197ec2d2d3479b39f6d2983a8eb7d85
Dmytro <v3il@ukr.net>
2023-09-16 13:25:36 +0300

    Retry settings loading in popup

--- src/background/background.js
+++ src/background/background.js
@@ -1,16 +1,5 @@
 import { SettingsService } from './SettingsService';
 
-chrome.runtime.onInstalled.addListener(async () => {
-    for (const contentScript of chrome.runtime.getManifest().content_scripts) {
-        for (const tab of await chrome.tabs.query({ url: contentScript.matches })) {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                files: contentScript.js
-            });
-        }
-    }
-});
-
 (async () => {
     const settingsService = new SettingsService();
 
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,8 +1,23 @@
+let retry = false;
+
+async function loadSettings() {
+    try {
+        return chrome.runtime.sendMessage({ action: 'LOAD_SETTINGS' });
+    } catch (error) {
+        console.error('retry');
+        loadSettings();
+        retry = true;
+    }
+}
+
 (() => {
     document.addEventListener('DOMContentLoaded', async () => {
-        const settings = await chrome.runtime.sendMessage({ action: 'LOAD_SETTINGS' });
-
+        const settings = await loadSettings();
         console.error(settings);
+
+        if (retry) {
+            document.body.style.backgroundColor = 'lightcoral';
+        }
 
         Object.entries(settings).forEach(([key, value]) => {
             const input = document.querySelector(`[data-prop="${key}"]`);
