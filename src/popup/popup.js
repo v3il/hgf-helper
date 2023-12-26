6b823361e15f68e7517fdba9bf8c1ab0a779cd23
Dmytro <v3il@ukr.net>
2023-12-26 17:10:50 +0200

    Refactor popup structure

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,9 +1,12 @@
+import './popup.css';
 import { SettingsService } from './SettingsService';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const settingsService = SettingsService.create();
 
     await settingsService.loadSettings();
+
+    console.info(settingsService.settings);
 
     Object.entries(settingsService.settings).forEach(([key, value]) => {
         const input = document.querySelector(`[data-prop="${key}"]`);
