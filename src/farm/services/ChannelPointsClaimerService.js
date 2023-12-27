1115c1822da3b282cbf559935a33f251eaf6ddbc
Dmytro <v3il@ukr.net>
2023-12-27 13:27:23 +0200

    Remove logs

--- src/farm/services/ChannelPointsClaimerService.js
+++ src/farm/services/ChannelPointsClaimerService.js
@@ -15,10 +15,6 @@ export class ChannelPointsClaimerService {
         this.#observer.observe(this.#chatInputContainerEl, { childList: true, subtree: true });
 
         this.#checkClaimButton();
-        this.#checkClaimButton();
-        this.#checkClaimButton();
-        this.#checkClaimButton();
-        this.#checkClaimButton();
     }
 
     #createObserver() {
@@ -34,11 +30,8 @@ export class ChannelPointsClaimerService {
     #checkClaimButton = debounce(() => {
         const claimButtonEl = this.#chatInputContainerEl.querySelector('[aria-label="Claim Bonus"]');
 
-        console.error('check');
-
         if (claimButtonEl) {
-            console.error('Claim button found, clicking it');
             claimButtonEl.click();
         }
-    }, 1000);
+    }, 2000);
 }
