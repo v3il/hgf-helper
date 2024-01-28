0cdc84308651d73cfc325578ef5783e79570b94f
Dmytro <v3il@ukr.net>
2024-01-28 14:35:32 +0200

    Improve decrease delay mechanism

--- src/farm/consts/GlobalVariables.js
+++ src/farm/consts/GlobalVariables.js
@@ -3,6 +3,6 @@ import { Timing } from './Timing';
 export const GlobalVariables = Object.freeze({
     ANTI_CHEAT_DURATION: 2 * Timing.MINUTE,
     HITSQUAD_GAMES_ON_SCREEN: 12,
-    PAGE_RELOAD_ROUNDS: 10,
-    DECREASE_DELAY_ROUNDS: 20
+    PAGE_RELOAD_ROUNDS: 30,
+    DECREASE_DELAY_TIMEOUT: 30 * Timing.SECOND
 });
