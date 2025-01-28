5dab30155bb95db153939e324e0703eb85ed1b9b
v3il <v3il@ukr.net>
2025-01-28 20:56:44 +0200

    fix description

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'wxt';
 import { buildConfig } from './wxt-common.config';
 
 export default defineConfig(buildConfig({
-    description: 'HGF Helper (build {{version}})',
+    description: 'HGF Helper',
 
     zip: {
         artifactTemplate: 'hgf-helper@v{{version}}.zip'
