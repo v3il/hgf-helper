d23c52af22e09cf1ceff9da4f3f18d249cd71b8e
v3il <v3il@ukr.net>
2025-04-29 10:19:35 +0300

    Update mini games compact mode

--- entrypoints/twitch.content/views/miniGames/basicComponents/MiniGamesPanel.svelte
+++ entrypoints/twitch.content/views/miniGames/basicComponents/MiniGamesPanel.svelte
@@ -1,12 +1,12 @@
 <div class={classes}>
-    <div class="flex items-center gap-3">
+    <div class="flex items-center gap-[12px]">
         <Icon size="20" class="text-[#9b87f5]" />
         <h4 class="font-semibold text-[#a1a1aa] text-[16px]">{name}</h4>
 
         {@render gameIndicators()}
     </div>
 
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-[16px]">
         <Switch isChecked={isRunning} onChange={onCheckboxChange} />
 
         <div class="shrink-0 w-[1px] h-[32px] ml-[8px] bg-[#27272a]"></div>
@@ -15,7 +15,7 @@
             onclick={participate}
             class="inline-flex items-center justify-center gap-[8px] whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-[40px] px-[16px] py-[8px] uppercase text-[#a1a1aa] hover:bg-[#27272a]/50 hover:text-[#d4d4d8] text-[14px]"
         >
-            <Play size="16" class="text-green-500" />
+            <Send size="16" class="text-green-500" />
             SEND
         </button>
     </div>
@@ -25,7 +25,7 @@
 import { Switch } from '@shared/components';
 import type { Component, Snippet } from 'svelte';
 import clsx from 'clsx';
-import { Play } from '@lucide/svelte';
+import { Send } from '@lucide/svelte';
 
 interface Props {
     isRunning: boolean;
@@ -41,7 +41,7 @@ let { gameIndicators, Icon, onCheckboxChange, participate, isRunning, name, ...r
 
 const classes = $derived(
     clsx([
-        'w-full bg-[#18181b]/80 backdrop-blur-sm shadow-lg space-x-4 flex items-center justify-between px-[16px] py-[8px]',
+        'w-full bg-[#18181b]/80 backdrop-blur-sm shadow-lg space-x-4 flex items-center justify-between px-[16px] py-[4px]',
         rest.class ?? ''
     ])
 );
--- entrypoints/twitch.content/views/miniGames/compact/CompactMiniGamesControlsItem.svelte
+++ entrypoints/twitch.content/views/miniGames/compact/CompactMiniGamesControlsItem.svelte
@@ -1,15 +1,22 @@
 <div class="flex items-center gap-2">
-    <div class="relative">
+    <div class="relative flex items-center gap-[8px] justify-center bg-[#27272a]/20 rounded-lg p-[2px]">
         <button
-            class="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground bg-[#27272a]/20 hover:bg-[#27272a]/40 border border-[#3f3f46]/30 rounded-lg p-[12px] h-auto w-auto transition-all duration-200 group"
+            class="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground bg-[#27272a]/20 hover:bg-[#27272a]/40 border border-[#3f3f46]/30 rounded-lg p-[8px] h-auto w-auto transition-all duration-200 group"
             title="Giveaways"
             onclick={onToggle}
         >
             <Icon size="16" class="text-[#9b87f5]"/>
         </button>
 
+        <button
+            class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:text-accent-foreground hover:text-accent-foreground bg-[#27272a]/20 hover:bg-[#27272a]/40 border border-[#3f3f46]/30 rounded-r-lg p-1.5 h-auto w-auto transition-all duration-200 group data-[state=on]:bg-[#27272a]/50"
+            title="Participate in Giveaways" tabindex="-1"
+        >
+            <Send size="14" class="text-green-500" />
+        </button>
+
         {#if isTimerVisible}
-            <div class="rounded-full border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 absolute -top-[4px] -right-[8px] text-[10px] h-[12px] p-[4px] flex items-center justify-center bg-[#221F26] text-[#9b87f5] border-none font-mono">
+            <div class="rounded-full border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 absolute -top-[4px] -left-[8px] text-[10px] h-[12px] p-[4px] flex items-center justify-center bg-[#221F26] text-[#9b87f5] border-none font-mono">
                 <MiniGamesTimer {timeout}/>
             </div>
         {/if}
@@ -18,7 +25,8 @@
 
 <script lang="ts">
 import type { Component } from 'svelte';
-import { MiniGamesTimer } from '../basicComponents'
+import { MiniGamesTimer } from '../basicComponents';
+import { Send } from '@lucide/svelte';
 
 interface Props {
     Icon: Component;
