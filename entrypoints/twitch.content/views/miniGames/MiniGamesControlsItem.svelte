aa6941e33d3483f7766c89291d310e83c24d10e0
v3il <v3il@ukr.net>
2025-06-20 22:20:47 +0300

    Update buttons

--- entrypoints/twitch.content/views/miniGames/MiniGamesControlsItem.svelte
+++ entrypoints/twitch.content/views/miniGames/MiniGamesControlsItem.svelte
@@ -1,6 +1,6 @@
 <div class="relative flex items-center justify-center rounded-lg">
     <button
-        class="inline-flex items-center justify-center h-[40px] w-[40px] dark:hover:bg-[#27272a]/40 border border-[#3232399e]/70 dark:border-[#3f3f46]/30 rounded-l-lg p-[8px] transition-all duration-200 group ring-offset-background disabled:pointer-events-none disabled:opacity-50 dark:hover:text-accent-foreground"
+        class="inline-flex items-center justify-center h-[40px] w-[40px] dark:hover:bg-[#27272a]/40 border border-[#3232399e]/70 dark:border-[#3f3f46]/30 rounded-l-lg p-[8px] transition-all duration-200 group ring-offset-background disabled:opacity-50 disabled:cursor-not-allowed dark:hover:text-accent-foreground"
         title={name}
         onclick={() => toggle(!isGameActive)}
     >
@@ -9,7 +9,7 @@
 
     <div title={sendButtonTooltip}>
         <button
-            class="inline-flex items-center justify-center h-[40px] w-[40px] dark:hover:bg-[#27272a]/40 border border-[#3232399e]/70 border-l-0 dark:border-[#3f3f46]/30 rounded-r-lg p-[8px] transition-all duration-200 group ring-offset-background disabled:pointer-events-none disabled:opa1city-50 dark:hover:text-accent-foreground"
+            class="inline-flex items-center justify-center h-[40px] w-[40px] dark:hover:bg-[#27272a]/40 border border-[#3232399e]/70 border-l-0 dark:border-[#3f3f46]/30 rounded-r-lg p-[8px] transition-all duration-200 group ring-offset-background disabled:cursor-not-allowed dark:hover:text-accent-foreground"
             tabindex="-1"
             onclick={() => participate()}
             disabled={!isSendEnabled}
@@ -41,7 +41,7 @@ const streamStatusService = Container.get(StreamStatusService);
 
 let { Icon, isGameActive, isSendEnabled, name, toggle, participate, indicators }: Props = $props();
 
-const toggleIconClasses = $derived(isGameActive ? 'text-[#8456FF] dark:text-[#9b87f5] group-hover:text-[#9b87f5]' : 'text-gray-400');
+const toggleIconClasses = $derived(isGameActive ? 'text-[#8456FF] dark:text-[#9b87f5] group-hover:text-[#9b87f5]' : 'text-[#53535f] dark:text-gray-400');
 const sendIconClasses = $derived(isSendEnabled ? 'text-green-700 dark:text-green-500 group-hover:text-green-600' : 'text-[#53535f]');
 
 const sendButtonTooltip = $derived.by(() => {
